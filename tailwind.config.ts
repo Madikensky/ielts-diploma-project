5efc896063bd7712eac1348403c8f9fb3c3be3f4
Madiyar <madikensky2003@gmail.com>
2025-02-19 17:51:15 +0500

    fix: fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,53 +7,54 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/entities/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			textCommon: '#C95559',
-  			textWhite: '#fff',
-  			textBlack: '#000',
-  			textCorrectAnswer: '#389E48',
-  			textWrongAnswer: '#EB2930'
-  		},
-  		backgroundColor: {
-  			bgCommonShadowed: '#96393C',
-  			bgCommon: '#C95559',
-  			bgWhite: '#fff',
-  			bgBlack: '#000',
-  			bgCommonLight: '#E9777B'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        textCommon: "#C95559",
+        textWhite: "#fff",
+        textBlack: "#000",
+        textCorrectAnswer: "#389E48",
+        textWrongAnswer: "#EB2930",
+      },
+      backgroundColor: {
+        bgCommonShadowed: "#96393C",
+        bgCommon: "#C95559",
+        bgWhite: "#fff",
+        bgBlack: "#000",
+        bgCommonLight: "#E9777B",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
   // corePlugins: {
