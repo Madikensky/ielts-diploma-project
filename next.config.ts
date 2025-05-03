e1fb0a72e50384f7681cd5fd2c7fe2f40cc8c585
Madiyar <madikensky2003@gmail.com>
2025-05-03 13:54:35 +0500

    fix: fix config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_API}:path*`
+      }
+    ]
+  }
   /* config options here */
 };
 
